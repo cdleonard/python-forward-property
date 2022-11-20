5e20a512cd0a72d2fb2c2b3bbb3cb14b2a73422d
Leonard Crestez <cdleonard@gmail.com>
2022-11-20 14:12:46 +0200

    forward_property: Add typed_forward_property
    Signed-off-by: Leonard Crestez <cdleonard@gmail.com>

--- forward_property.py
+++ forward_property.py
@@ -4,6 +4,8 @@ Module implementing property forwarding for python objects
 
 __version__ = "0.1.0"
 
+from typing import Generic, TypeVar
+
 
 class forward_property:
     """A descriptor for property forwarding"""
@@ -20,8 +22,33 @@ class forward_property:
     def __get__(self, outer, owner=None):
         return getattr(getattr(outer, self.inner), self.prop)
 
-    def __set__(self, outer, value):
+    def __set__(self, outer, value) -> None:
         setattr(getattr(outer, self.inner), self.prop, value)
 
-    def __delete__(self, outer):
+    def __delete__(self, outer) -> None:
+        delattr(getattr(outer, self.inner), self.prop)
+
+
+GetSetType = TypeVar("GetSetType")
+
+
+class typed_forward_property(Generic[GetSetType]):
+    """A descriptor for property forwarding"""
+
+    __slots__ = ("inner", "prop")
+
+    inner: str
+    prop: str
+
+    def __init__(self, attr: str, prop: str):
+        self.inner = attr
+        self.prop = prop
+
+    def __get__(self, outer, owner=None) -> GetSetType:
+        return getattr(getattr(outer, self.inner), self.prop)
+
+    def __set__(self, outer, value: GetSetType) -> None:
+        setattr(getattr(outer, self.inner), self.prop, value)
+
+    def __delete__(self, outer) -> None:
         delattr(getattr(outer, self.inner), self.prop)
